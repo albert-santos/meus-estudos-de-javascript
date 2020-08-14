14d51cd9177d6049a4d712f0344f36fcadd0a225
Albert <alberteinsteincds@gmail.com>
2020-08-14 11:59:29 -0300

    Criando uma instância de Negociacao

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,17 +2,22 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
 
     adiciona(event) {
         event.preventDefault();
 
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        let negociacao = new Negociacao(
+            this._inputQuantidade.value;
+            this._inputQuantidade.value;
+            this._inputValor.value;
+        );
+
+        console.log(negociacao);
+
     }
 }
