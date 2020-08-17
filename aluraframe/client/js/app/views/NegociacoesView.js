ceb676d3f73111cc9dc8ad69da49588984747fb6
Albert <alberteinsteincds@gmail.com>
2020-08-17 11:05:49 -0300

    Utilizando herança e reutilização de código

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View{
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template (model) {
@@ -42,9 +42,6 @@ class NegociacoesView {
 
     }
 
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
-    }
 }
 
 
