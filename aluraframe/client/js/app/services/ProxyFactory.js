38cb5964610a0643b8893bb3ed1271d67713ecda
Albert <alberteinsteincds@gmail.com>
2020-08-19 12:11:44 -0300

    Melhorando o proxy

--- aluraframe/client/js/app/services/ProxyFactory.js
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -1,9 +1,9 @@
 class ProxyFactory {
-    static create(objetp, props, acao) {
-        return new Proxy(new ListaNegociacoes(), {
+    static create(objeto, props, acao) {
+        return new Proxy(objeto, {
     
             get(target, prop, receiver) {
-                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
                     return function() {
                         console.log(`a propriedade "${prop}" foi interceptada`);
                         Reflect.apply(target[prop], target, arguments);
@@ -11,8 +11,22 @@ class ProxyFactory {
                     }
                 }
                 return Reflect.get(target, prop, receiver);
+            },
+            set(target, prop, value, receiver) {
+                if(props.includes(prop)) {
+                    target[prop] = value;
+                    acao(target);
+                }
+            
+                return Reflect.set(target, prop, value, receiver);
             }
+
+
         });
 
+    }
+
+    static _ehFuncao(func) {
+        return typeof(func) == typeof(Function);
     }
 }
