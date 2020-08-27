63f4c9c6a352f9bee7a8ccf3e1f4db859aa57ed0
Albert <alberteinsteincds@gmail.com>
2020-08-27 12:38:56 -0300

    Criando stores

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -2,6 +2,7 @@ var stores = ['negociacoes'];
 var version = 4;
 var dbName = 'aluraframe';
 
+var connection = null;
 
 class ConnectionFactory{
 
@@ -11,22 +12,37 @@ class ConnectionFactory{
 
     static getConnection() {
 
-        return Promise((resolve, reject) => {
-            let openRequest = window.indexedDB.open(dbName, version);
+        return new Promise((resolve, reject) => {
+            let openRequest = window.indexedDB.open(dbName,version);
 
             openRequest.onupgradeneeded = e => {
-
+                
+                ConnectionFactory._createStores(e.target.result);
 
             };
 
             openRequest.onsuccess = e => {
 
+                resolve(e.target.result);
+
             };
 
             openRequest.onerror = e => {
+                console.log(e.target.error);
 
+                reject(e.target.error.name);
             };
 
+        });
+    }
+
+    static _createStores(connection) {
+
+        stores.forEach(store => {
+            if (connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store);
+
+            connection.createObjectStore(store, {autoIncrement: true});
         });
     }
 }
